e9084b3115e5f6d9fc857d5d5bc4c25ce64a68bd
Michael Holachek <holachek@mit.edu>
2017-01-18 22:14:05 -0500

    Latest working test of SSD1331.

--- OLEDPowerCharaterization.cydsn/main.c
+++ OLEDPowerCharaterization.cydsn/main.c
@@ -12,15 +12,40 @@ uint8_t I2CM_WriteBuf[100];
 
 char output[100];
 
-uint16_t makeColor(uint8_t r, uint8_t g, uint8_t b)
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+// make 24-bit color into 16-bit color (5-bit r, 6-bit g, 5-bit b)
+uint16_t makeColor888(uint8_t r, uint8_t g, uint8_t b)
 {
     uint16_t c;
+    
     c = r >> 3;
     c <<= 6;
     c |= g >> 2;
     c <<= 5;
     c |= b >> 3;
 
+    return c;
+}
+
+// make 16-bit color into 16-bit color
+uint16_t makeColor565(uint8_t r, uint8_t g, uint8_t b)
+{
+    uint16_t c;
+    
+    c = r;
+    c <<= 6;
+    c |= g;
+    c <<= 5;
+    c |= b;
+    
     return c;
 }
 
@@ -39,9 +64,10 @@ void drawRectangle(uint16_t fillColor){
     SPIM_WriteTxData(0x00);
     SPIM_WriteTxData(0x00);
     
-    SPIM_WriteTxData((uint8_t)((fillColor >> 11) << 1)); // fill colors by calculating R, G, B from 16-bit number
-    SPIM_WriteTxData((uint8_t)((fillColor >> 5) & 0x3F));
-    SPIM_WriteTxData((uint8_t)((fillColor << 1) & 0x3F));
+    // fill colors by calculating R, G, B from 16-bit number
+    SPIM_WriteTxData((uint8_t)((fillColor >> 11) << 1)); // red = [5:1]
+    SPIM_WriteTxData((uint8_t)((fillColor >> 5) & 0x3F)); // green = [5:0]
+    SPIM_WriteTxData((uint8_t)((fillColor << 1) & 0x3F)); // blue = [5:1]
     
     CyDelay(10);
     
@@ -51,13 +77,13 @@ void drawRectangle(uint16_t fillColor){
 // http://academe.co.uk/wp-content/uploads/2012/04/451px-HSV-RGB-comparison.svg_.png
 uint16_t Wheel(uint8_t WheelPos) {
   if(WheelPos < 85) {
-   return makeColor(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return makeColor888(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if(WheelPos < 170) {
    WheelPos -= 85;
-   return makeColor(255 - WheelPos * 3, 0, WheelPos * 3);
+   return makeColor888(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
    WheelPos -= 170;
-   return makeColor(0, WheelPos * 3, 255 - WheelPos * 3);
+   return makeColor888(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
 
@@ -132,11 +158,11 @@ void InitSSD1351OLED() {
     SPIM_WriteTxData(SSD1331_CMD_MASTERCURRENT);
     SPIM_WriteTxData(0x06);
     SPIM_WriteTxData(SSD1331_CMD_CONTRASTA);
-    SPIM_WriteTxData(0xFF); // default 0x91
+    SPIM_WriteTxData(0x91); // default 0x91
     SPIM_WriteTxData(SSD1331_CMD_CONTRASTB);
-    SPIM_WriteTxData(0xFF); // default x050
+    SPIM_WriteTxData(0x50); // default x050
     SPIM_WriteTxData(SSD1331_CMD_CONTRASTC);
-    SPIM_WriteTxData(0xFF); // default 0x7D
+    SPIM_WriteTxData(0x7D); // default 0x7D
     SPIM_WriteTxData(SSD1331_CMD_DISPLAYON);   
 }
 
@@ -205,18 +231,28 @@ int main()
     while(I2CM_I2CMasterStatus() == I2CM_I2C_MSTAT_XFER_INP);
         
     int r, g, b;
+    uint8_t red32, green64, blue32;
+    uint16_t current_color;
     
     for(;;)
     {
         
         // 16-bit color read
-        for (r=0; r<32; r++){
-            for (g=0; g<64; g++){
-                for (b=0; b<32; b++){
+        for (r=0; r<3; r++){
+            for (g=0; g<32; g++){
+                // for (b=0; b<32; b++){
+            
+                    if(r==0){
+                        current_color = makeColor565(g, 0, 0);
+                    } else if (r==1){
+                        current_color = makeColor565(0, g*2, 0);
+                    } else if (r==2){
+                        current_color = makeColor565(0, 0, g);   
+                    }
                     
-                    uint16_t current_color = makeColor(r, g, b);
+                    // uint16_t current_color = makeColor565(r, g, b);
                     drawRectangle(current_color);  
-                    CyDelay(20);
+                    CyDelay(35);
                 
                     // Read current register location
                     I2CM_I2CMasterReadBuf(INA219_ADDRESS, I2CM_ReadBuf, 2, I2CM_I2C_MODE_REPEAT_START);
@@ -234,18 +270,28 @@ int main()
                     // sprintf(output, "Wheel Position: %i\t\t", j);
                     
                     // MegunoLink XY plotting syntax: {XYPLOT|DATA|Series|yourXdata|yourYdata}
-                    sprintf(output, "{XYPLOT|DATA|Series|%i", current_color);
-                    UART_UartPutString(output);
+                    // sprintf(output, "{XYPLOT|DATA|Series|%i", current_color);
+                    // sprintf(output, "{XYPLOT|DATA|Series|%i", g);
+                    // UART_UartPutString(output);
                     
                     // sprintf(output, "Shunt Voltage: %.2f mV\r\n\n", shunt_voltage);
-                    sprintf(output, "|%.3f}\t\t", display_current);
+                    sprintf(output, "%.3f, ", display_current);
                     UART_UartPutString(output);
+                    
+                    if(g == 0){
+                     sprintf(output, "\r\n\n\n%i\n", r);
+                     UART_UartPutString(output);
+                    }
+                    
+                    red32 = ((current_color >> 11)); // red = [4:0]
+                    green64 = ((current_color >> 5) & 0x3F); // green = [5:0]
+                    blue32 = ((current_color) & 0x1F); // blue = [4:0]
                     
                     // print out r, g, b values
-                    sprintf(output, "[r:%i, g:%i, b:%i]\r\n", r, g, b);
-                    UART_UartPutString(output);
+                    //sprintf(output, "[r:%i, g:%i, b:%i]\r\n", red32, green64, blue32);
+                    //UART_UartPutString(output);
                     
-                } // end b loop
+                // } // end b loop
             } // end g loop
         } // end r loop
         
